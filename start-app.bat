060f3e0a6b2b91a4e65c3d9e3fda7f22bcba2de3
drfoehn <janne.unger@gmail.com>
2025-06-20 20:55:19 +0200

    added batch file

---
+++ start-app.bat
@@ -0,0 +1,8 @@
+@echo off
+REM This script opens the project's index.html file in the default web browser.
+
+REM The "start" command opens a file with its default application.
+REM The empty "" is a placeholder for the window title, which is a quirk of the start command needed for paths with spaces.
+start "" "C:\.......\index.html"
+
+exit 
